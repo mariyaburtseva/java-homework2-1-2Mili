118bda2ec9dc2dfd7d622573e53559e57cd129ff
Burtseva Mariya <mariyaburceva9898@mail.ru>
2020-12-21 12:49:24 +0300

    "Added Main.java"

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 10_500;
+        int miles = priceTicket / 20;
+        System.out.println(miles);
+    }
+}
+
+
+
